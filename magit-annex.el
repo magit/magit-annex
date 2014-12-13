8f927ea37ab141a39727114dea14f1912629c5b6
Kyle Meyer <kyle@kyleam.com>
2014-12-12 19:40:46 -0500

    magit-annex-log-unused: Catch up with upstream
    magit-log-refresh-buffer now takes a list of revs.  See Magit commit
    5c0dc560f7d40f98b511a33e253499bed2a4d038.

--- magit-annex.el
+++ magit-annex.el
@@ -470,7 +470,7 @@ called instead.
        (magit-mode-setup magit-log-buffer-name-format nil
                          #'magit-log-mode
                          #'magit-log-refresh-buffer
-                         'verbose "HEAD" (list "--stat" "-S" key))))
+                         'verbose '("HEAD") (list "--stat" "-S" key))))
     (t (magit-log-popup))))
 
 (defcustom magit-annex-unused-sections-hook
