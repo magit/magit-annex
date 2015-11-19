0e61512f5364006885871afa2ae0e9c8d2886654
Kyle Meyer <kyle@kyleam.com>
2015-11-18 23:47:17 -0500

    Update minimum version for Magit
    Oops, that's been stale for a while.

--- magit-annex.el
+++ magit-annex.el
@@ -7,7 +7,7 @@
 ;; URL: https://github.com/kyleam/magit-annex
 ;; Keywords: vc tools
 ;; Version: 1.0.0
-;; Package-Requires: ((cl-lib "0.3") (magit "1.2.0"))
+;; Package-Requires: ((cl-lib "0.3") (magit "2.3.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
