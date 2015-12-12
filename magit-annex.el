d1ca40e8d0a68871ec516570064a70459827b39d
Kyle Meyer <kyle@kyleam.com>
2015-12-12 18:24:37 -0500

    Update copyright years

--- magit-annex.el
+++ magit-annex.el
@@ -1,6 +1,6 @@
 ;;; magit-annex.el --- Control git-annex from Magit
 
-;; Copyright (C) 2013 Kyle Meyer <kyle@kyleam.com>
+;; Copyright (C) 2013-2015 Kyle Meyer <kyle@kyleam.com>
 
 ;; Author: Kyle Meyer <kyle@kyleam.com>
 ;;         Rémi Vanicat <vanicat@debian.org>
