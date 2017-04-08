ae6250aead8f283eac8d6951a249cfc7d85e0b40
Kyle Meyer <kyle@kyleam.com>
2017-04-08 17:26:33 -0400

    Bump version

--- magit-annex.el
+++ magit-annex.el
@@ -6,7 +6,7 @@
 ;;         Rémi Vanicat <vanicat@debian.org>
 ;; URL: https://github.com/kyleam/magit-annex
 ;; Keywords: vc tools
-;; Version: 1.3.0
+;; Version: 1.3.1
 ;; Package-Requires: ((cl-lib "0.3") (magit "2.3.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
