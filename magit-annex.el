cd725b284cc59cf4f3b335b1d285d62971739df8
Kyle Meyer <kyle@kyleam.com>
2018-04-16 18:00:25 -0400

    Bump Magit version requirement
    Eh, I should've done that with the release of 2.12.
    
    The dependency on 2.12 is due to 06ca472 (Use oref instead of
    dedicated slot accessors, 2017-12-15).

--- magit-annex.el
+++ magit-annex.el
@@ -7,7 +7,7 @@
 ;; URL: https://github.com/magit/magit-annex
 ;; Keywords: vc tools
 ;; Version: 1.4.0
-;; Package-Requires: ((cl-lib "0.3") (magit "2.11.0"))
+;; Package-Requires: ((cl-lib "0.3") (magit "2.12.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
